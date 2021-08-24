d61cf58a96eae0a22d95db0de81dbc1015d214b2
Halil Ibrahim Kaya <halilibrahim280@gmail.com>
2021-08-24 02:45:54 +0200

    code refactored and issues solved

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -37,25 +37,26 @@ router.get("/article/:id", (req, res) => {
 
 router.post("/create", (req, res, next) => {
   const { section, subsection, title, body, created_date, image } = req.body;
+  const { _id } = req.session.loggedInUser;
   ArticleModel.create({
     section: section,
     subsection: subsection,
     title: title,
     body: body,
     created_date: created_date,
-    author: req.session.loggedInUser,
+    author: _id,
     image: image,
   }).then((response) => {
-    res.status(200).json(response);
-    UserModel.findByIdAndUpdate(req.session.loggedInUser._id, {
+    UserModel.findByIdAndUpdate(_id, {
       $push: { articles: response._id },
-    })
-      .then(() => {
-        console.log("article added to user");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    }).then(() => {
+      res.status(200).json(response);  
+    })   
+  }).catch ((err) => {
+    res.status(500).json({
+      error: "Something went wrong",
+      message: err,
+    });
   });
 });
 
